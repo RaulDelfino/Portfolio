7799d9b1d4055b81aa19bf1d714862a5f692dba8
Raul Delfino <rauldossantoss@gmail.com>
2022-12-30 00:12:16 -0300

    add img

--- src/components/sobre/index.jsx
+++ src/components/sobre/index.jsx
@@ -11,7 +11,9 @@ export function Sobre(){
                      known printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker .
                      </p>
             </div>
-            <img src='' alt='Foto Perfil' className='foto'></img>
+                <div>
+                    <img src='Raulfoto.jfif' alt='Foto Perfil' className='fotoPerfil'></img>
+                </div>
         </div>
     )
 }
